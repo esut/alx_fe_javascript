d6998c99685dacafdfcd4b1a828a8d76232682c5
unknown <mounir17xm@gmail.com>
2024-08-04 23:37:36 +0100

    add script.js

---
+++ dom-manipulation/script.js
@@ -0,0 +1,17 @@
+const quotes = [
+    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "Get busy living or get busy dying.", category: "Motivation" },
+  ];
+  
+  
+  function showRandomQuote() {
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    quoteDisplay.innerHTML = `<p>${randomQuote.text}</p><p><em>${randomQuote.category}</em></p>`;
+  }
+  
+  //show new quote
+  document.getElementById('newQuote').addEventListener('click', showRandomQuote);
+  
